1ea05afeeea2867a99d599db9a2f7c4d5d5bda0c
efpi <efpe@efpee>
2021-07-28 20:34:23 +0200

    umm it works

--- ytpmv.py
+++ ytpmv.py
@@ -1,60 +1,65 @@
 import ffmpeg
 import os
 import shutil
+from moviepy.editor import *
 
 
 if os.path.exists('assets'):
 	shutil.rmtree('assets')
-	os.mkdir('assets')
-	shutil.copy('samp1.mp4', 'assets')
+os.mkdir('assets')
+shutil.copy('samp1.mp4', 'assets')
 
 inFlip = ffmpeg.input('samp1.mp4')
 inFlipVid = inFlip.video.hflip()
-inFlipAudio = inFlip.audio
-outFlip = ffmpeg.output(inFlipVid, inFlipAudio, 'assets/samp-1.mp4')
+outFlip = ffmpeg.output(inFlipVid, 'assets/samp-1.mp4')
 outFlip.run()
-
 
 bpm = 120
 
-notes = ['0/1', '0/1', '0/1', '0/1', '0/0.5', '0/0.5', '0/1', '0/1', '0/1', '0/1', '0/0.5', '0/0.5', '2/0.25', '/0.25', '4/0.25', '6/0.25', '8/0.25', '10/0.25']
+notes = ['0/0.5', '0/0.5', '0/0.5', '2/0.5', '5/0.5', '7/10', '4/1', '4/0.5', '4/0.5', '9/0.5']
 
 uniqueNotes = []
 for i in notes:
 	notePitch = i.split('/')[0]
 	if notePitch != '' and int(notePitch) not in uniqueNotes:
 		uniqueNotes.append(int(notePitch))
-print(uniqueNotes)
 
 for i in uniqueNotes:
-	pitchedSample = ffmpeg.input('samp1.mp4').audio.filter('rubberband', pitch=1*2**(i/12))
+	pitchedSample = ffmpeg.input('samp1.mp4').audio.filter('rubberband', pitch=2**(i/12))
 	out = ffmpeg.output(pitchedSample, f'assets/samp{i}.mp3')
 	out.run()
 
 
-timeline = []
+timelineV = []
+timelineA = []
 flipSwitch = 1
-
+timer = 0.0
 for i in range(len(notes)):
 
-	length = float(notes[i].split('/')[1])
+	length = float(notes[i].split('/')[1])*60/bpm
 	pitch = notes[i].split('/')[0]
 
-	if pitch == '':
-		inAudio = ffmpeg.input('samp1.mp4', t=length*60/bpm).audio.filter('volume', 0)
 
+	audio = AudioFileClip(f'assets/samp{pitch}.mp3')
+	clip = VideoFileClip(f"assets/samp{flipSwitch}.mp4")
+
+	clip.start = timer
+	audio.start = timer
+	if length < clip.duration:
+		clip.end = timer+length
+		audio.end = timer+length
 	else:
-		inVid = ffmpeg.input(f'assets/samp{flipSwitch}.mp4', t=length*60/bpm).video
-		flipSwitch *= -1
-		inAudio = ffmpeg.input(f'assets/samp{pitch}.mp3', t=length*60/bpm).audio
+		clip.end = timer+clip.duration
+		audio.end = timer+clip.duration
 
-	timeline.append(inVid)
-	timeline.append(inAudio)
+	timer += length
+	flipSwitch *= -1
 
-joined = ffmpeg.concat(*timeline, v=1, a=1).node
-video = joined[0]
-audio = joined[1]
+	timelineV.append(clip)
+	timelineA.append(audio)
 
-out = ffmpeg.output(video, audio, 'assets/out.mp4')
-out.run()
+final_Vclip = CompositeVideoClip(timelineV)
+final_Aclip = CompositeAudioClip(timelineA)
+final_Vclip.audio = final_Aclip
+final_Vclip.resize(width=320).write_videofile("mudzinmv.mp4")
 
